733fae8a145616b98cdbbc7a180010d01fcdf682
Justin Walker <walkerjt93@gmail.com>
2020-02-10 13:32:30 -0500

    trying to get everything configured to work with prettier, and set local workspace vscode settings so I dont have to change the global ones

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: "app-game-control",
-  templateUrl: "./game-control.component.html",
-  styleUrls: ["./game-control.component.css"]
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
   @Output() intervalFired = new EventEmitter<number>();
