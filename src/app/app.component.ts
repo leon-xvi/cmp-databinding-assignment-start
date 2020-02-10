a5301f83c94b3d8c60ac41a31f2076ef7d00404a
justin <walkerjt93@gmail.com>
2020-02-09 20:05:10 -0500

    got through item # 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,8 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  onGameStarted() {
+    console.log("app component is picking up on event?");
+  }
+}
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-game-control',
-  templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  selector: "app-game-control",
+  templateUrl: "./game-control.component.html",
+  styleUrls: ["./game-control.component.css"]
 })
 export class GameControlComponent implements OnInit {
+  @Output() gameStarted = new EventEmitter<{ count: number }>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onGameStarted() {
+    let count = 0;
+    setInterval(() => {
+      count += 1;
+      console.log("count: ", count);
+    }, 1000);
+    this.gameStarted.emit({
+      count
+    });
   }
-
 }
