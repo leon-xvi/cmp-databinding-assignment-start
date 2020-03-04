493bea7ac5410550d825bd2e46f503849155251d
Justin Walker <walkerjt93@gmail.com>
2020-03-04 13:34:28 -0500

    moved the even/odd components inside of the game-control folder as it makes more sense this way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EvenComponent } from './game-control/even/even.component';
 import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
+import { OddComponent } from './game-control/odd/odd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameControlComponent,
-    OddComponent,
-    EvenComponent
+    EvenComponent,
+    OddComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
